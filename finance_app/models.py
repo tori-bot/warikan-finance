3873eacd15b3e2574281129259542a624bfb6798
victoria makena <makenavictoria1@gmail.com>
2022-06-23 11:25:56 +0300

    models: add profilemodel

--- finance_app/models.py
+++ finance_app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(User,primary_key=True,on_delete=models.CASCADE)   
+    profile_picture=models.ImageField(upload_to='profile_pictures/',default='default.jpg',null=True) 
+    bio=models.TextField()
+    
+    
+    @receiver(post_save, sender=User) 
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(self,user,profile_picture,bio):
+        self.user=user
+        self.profile_picture=profile_picture
+        self.bio=bio
+        self.save()
+
+    @classmethod
+    def get_profile_by_id(cls,id):
+        profile = Profile.objects.filter(user__id = id).first()
+        return profile
+
+    @classmethod
+    def search_profile(cls,search_term):
+        profile=cls.objects.filter(user__username__icontains=search_term).all()
+        return profile
+
+    def __str__(self):
+        return self.user
+
