df7d25e86e0895feb2b4b8525f19f65bab7a3d50
victoria makena <makenavictoria1@gmail.com>
2022-06-24 01:23:49 +0300

    api: make request to youtube api

--- finance_app/requests.py
+++ finance_app/requests.py
@@ -11,4 +11,9 @@ def get_all_news():
             news_list.append(item)
     return news_list
 
-    
+def youtube_trailer(title):
+    search_query = title.replace(' ', '+')
+    url=f'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q={search_query}&key={config("YOUTUBE_API")}'
+    response=requests.get(url)
+    youtube_id = response.json()['items'][0]['id']['videoId']
+    return youtube_id
